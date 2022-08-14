36e243587c5df53fa8a76fb70f6044320b535083
Heeyoun Jeong <90598889+codenamehee@users.noreply.github.com>
2022-08-14 20:16:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.esj3ket.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta
+
 @app.route('/')
 def home():
    return render_template('index.html')
 
 @app.route("/mars", methods=["POST"])
 def web_mars_post():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST 연결 완료!'})
+    name_receive = request.form['name_give']
+    address_receive = request.form['address_give']
+    size_receive = request.form['size_give']
+    doc = {
+        'name':name_receive
+        'address':address_receive
+        'size':size_receive
+    }
+    db.mars.insert_one(doc)
+
+
+    return jsonify({'msg': '주문 완료!'})
 
 @app.route("/mars", methods=["GET"])
 def web_mars_get():
